a90d793d5e1da61fb9546d593f474aa9f33b621a
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-06-05 18:28:23 +0300

    currency exchange via bootstrap angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+interface CurrencyRate {
+  rates: { [key: string]: number };
+  base: string; 
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'currency-exchange-angular';
+  currency1Amount = 0;
+  currency2Amount = 0;
+  currency1 = 'USD';
+  currency2 = 'EUR';
+  exchangeRate = 0;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getExchangeRate();
+  }
+
+  getExchangeRate() {
+    const url = `https://api.exchangerate-api.com/v4/latest/${this.currency1}`;
+    this.http.get<CurrencyRate>(url).subscribe(data => {
+      this.exchangeRate = data.rates[this.currency2];
+      if (this.currency2 === 'UAH') {
+        this.exchangeRate = 1 / this.exchangeRate; 
+      }
+      });
+  }
+
+  onCurrencyChange() {
+  this.getExchangeRate();
+}
+
+  convertCurrency() {
+    if (this.currency1Amount && this.exchangeRate) {
+      this.currency2Amount = this.currency1Amount * this.exchangeRate;
+    } else if (this.currency2Amount && this.exchangeRate) {
+      this.currency1Amount = this.currency2Amount / this.exchangeRate;
+    }
+    if (this.currency1Amount && this.exchangeRate) {
+      if (this.currency2 === 'UAH') {
+        this.currency2Amount = this.currency1Amount / this.exchangeRate;
+      } else {
+        this.currency2Amount = this.currency1Amount * this.exchangeRate;
+      }
+    } else if (this.currency2Amount && this.exchangeRate) {
+      if (this.currency1 === 'UAH') {
+        this.currency1Amount = this.currency2Amount / this.exchangeRate;
+      } else {
+        this.currency1Amount = this.currency2Amount * this.exchangeRate;
+      }
+    }    
+  }
 }
